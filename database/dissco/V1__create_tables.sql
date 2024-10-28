58c6aeb5d8228ad96b1ee821a3c2f09cdf4d5b61
southeo <soulaine.theo@gmail.com>
2024-10-28 11:49:14 +0100

    Feature/dissco exporter (#79)
    * add data exporter deployment
    
    * fix image name
    
    * fix image name
    
    * code review
    
    * code review
    
    * db
    
    * remove middleware

--- database/dissco/V1__create_tables.sql
+++ database/dissco/V1__create_tables.sql
@@ -30,6 +30,7 @@ create index annotation_id_target_id_index
 create type job_state as enum ('SCHEDULED', 'RUNNING', 'FAILED', 'COMPLETED');
 create type mjr_target_type as enum ('DIGITAL_SPECIMEN', 'MEDIA_OBJECT');
 create type error_code as enum ('TIMEOUT', 'DISSCO_EXCEPTION');
+create type export_type as enum ('DOI_LIST');
 
 create table mas_job_record
 (
@@ -192,3 +193,22 @@ create table translator_job_record
     PRIMARY KEY (job_id, source_system_id),
     FOREIGN KEY (source_system_id) REFERENCES source_system (id)
 );
+
+create table export_queue
+(
+    id                uuid                     not null
+        constraint export_queue_pk
+            primary key,
+    params            jsonb                    not null,
+    creator           text                     not null,
+    job_state         job_state                not null,
+    time_scheduled    timestamp with time zone not null,
+    time_started      timestamp with time zone,
+    time_completed    timestamp with time zone,
+    export_type       export_type              not null,
+    hashed_params     uuid                     not null,
+    destination_email text                     not null,
+    download_link     text,
+    target_type       text                     not null
+);
+
