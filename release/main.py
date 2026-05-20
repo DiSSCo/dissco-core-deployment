a94a58d6123aebb66b3d31772be9a426a2a28aae
Soulaine <soulaine.theo@gmail.com>
2026-05-20 15:40:28 +0200

    v1.5.0

--- release/main.py
+++ release/main.py
@@ -191,8 +191,8 @@ if __name__ == "__main__":
     * release_name (optional): Set this to the desired release name. If blank, release name will follow rules described in README
     """
     config = {
-        "env": None,
-        "do_update": False,
+        "env": Environment.PRODUCTION,
+        "do_update": True,
         "exclude_directories": DEFAULT_EXCLUDE_DIRECTORIES,
         "include_directories": [],
         "release_name": "",
